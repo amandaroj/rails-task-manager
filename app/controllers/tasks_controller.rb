fcdfbb9eb248c42668df35968cb9711d2de3fd89
amanda rojter <rojteramanda@gmail.com>
2022-09-22 17:25:44 -0400

    added bootstrap styling

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:title])
+    @task.save
+  end
+end
